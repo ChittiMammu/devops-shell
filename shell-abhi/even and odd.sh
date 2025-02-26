cc70ab452751c1fd65392fbe1d8a05d868ff72f5
sam <mallurusampath@gmail.com>
2025-02-26 15:34:11 +0530

    mamatha

---
+++ shell-abhi/even and odd.sh
@@ -0,0 +1,10 @@
+#! bin/bash
+
+# promptint the user to enter the number
+# user entered the number
+# check the number is divided by 2
+# if yes number is even
+# else number os odd
+
+echo "Enter the any number"
+read number 
