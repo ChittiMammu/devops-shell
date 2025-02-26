85ec6bb19901b77b9c7f5d74df54f44065513ecf
sam <mallurusampath@gmail.com>
2025-02-26 15:10:06 +0530

    mamatha

--- shell-abhi/server-response.sh
+++ shell-abhi/server-response.sh
@@ -2,4 +2,4 @@
 ## curl -Is https://facebook.com | head -n 1 | grep "200" > /dev/null && echo "Server is up" || echo "Server is down"
 # curl -Is https://facebook.com | head -n 8 | grep "200" && echo "Server is up" || echo "Server is down"
 # curl -Is https://facebook.com && echo "Server is up" || echo "Server is down"
-curl https://facebook.com && echo "Server is up" || echo "Server is down"
+curl https://facebook.com  echo "Server is up" || echo "Server is down"
